19a63d4749d829ba9c4631db22e40f94b1a8e63e
ShunyangLi0812 <z5139935@student.unsw.edu.au>
2019-10-09 14:49:58 +1100

    fix database

--- backend/util/init_database.sql
+++ backend/util/init_database.sql
@@ -1,64 +1,67 @@
 -- we use sqlite as the primary database
 
 -- create user table
+-- Host, Visitor
 CREATE TABLE IF NOT EXISTS User (
-    username TEXT PRIMARY KEY ,
-    password TEXT NOT NULL ,
-    user_type TEXT,
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL,
+    user_type TEXT NOT NULL,
     token TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Hotel (
-	  id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     phone TEXT NOT NULL,
     location TEXT NOT NULL,
     email TEXT,
-    price REAL NOT NULL ,
+    price REAL NOT NULL,
     web TEXT,
     description TEXT,
-    host TEXT NOT NULL ,
-    room_type TEXT NOT NULL ,
-    bathrooms REAL NOT NULL ,
-    bedrooms INTEGER NOT NULL ,
+    host TEXT NOT NULL,
+    room_type TEXT NOT NULL,
+    bathrooms REAL NOT NULL,
+    bedrooms INTEGER NOT NULL,
     FOREIGN KEY (host) REFERENCES User(username)
 );
 
 CREATE TABLE IF NOT EXISTS Hotel_img(
-    hotel_id INTEGER NOT NULL ,
-    url TEXT NOT NULL ,
+    hotel_id INTEGER NOT NULL,
+    url TEXT NOT NULL,
     FOREIGN KEY (hotel_id) REFERENCES Hotel (id)
 );
 
 CREATE TABLE IF NOT EXISTS Booking (
-    booking_id TEXT PRIMARY KEY ,
-    booking_date TEXT NOT NULL ,
-    check_in_date TEXT NOT NULL ,
-    days INTEGER NOT NULL ,
-    price REAL NOT NULL ,
-    room_type TEXT NOT NULL ,
+    booking_id TEXT PRIMARY KEY,
+    username TEXT NOT NULL,
+    passport TEXT NOT NULL,
+    booking_date TEXT NOT NULL,
+    check_in_date TEXT NOT NULL,
+    days INTEGER NOT NULL,
+    price REAL NOT NULL,
+    room_type TEXT NOT NULL,
     comment TEXT
 );
 
 
 CREATE TABLE IF NOT EXISTS Booking_hotel (
-    booking_id TEXT NOT NULL ,
-    hotel_id INTEGER NOT NULL ,
+    booking_id TEXT NOT NULL,
+    hotel_id INTEGER NOT NULL,
     FOREIGN KEY (booking_id) REFERENCES Booking(booking_id),
     FOREIGN KEY (hotel_id) REFERENCES Hotel (id)
 );
 
 CREATE TABLE IF NOT EXISTS User_booking (
-    booking_id TEXT NOT NULL ,
-    username TEXT NOT NULL ,
+    booking_id TEXT NOT NULL,
+    username TEXT NOT NULL,
     FOREIGN KEY (booking_id) REFERENCES Booking(booking_id),
     FOREIGN KEY (username) REFERENCES User(username)
 );
 
 
 CREATE TABLE IF NOT EXISTS Comments(
-    comment_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,
-    rating REAL ,
+    comment_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    rating REAL,
     comment TEXT,
     user_id TEXT,
     hotel_id INTEGER,
