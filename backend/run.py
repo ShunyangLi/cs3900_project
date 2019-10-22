6bdbda41c2706806059ba926bcb5f6a7f058e56a
ShunyangLi0812 <z5139935@student.unsw.edu.au>
2019-10-22 18:03:24 +1100

    add clean db api

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,16 @@
+import os
 from app import app
 import api.auth_handling
+
+
+@app.route('/cleanDB')
+def clean():
+    c = 'rm util/data.sqlite'
+    os.system(c)
+    init = 'sqlite3 util/data.sqlite < util/init_database.sql'
+    os.system(init)
+    return "success"
+
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True, host='127.0.0.1')
