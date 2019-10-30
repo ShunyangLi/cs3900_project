5f637124cd7dedb11ad419774753f7046ad137fa
Toby Zheng <tobbyzheng0807@gmail.com>
2019-10-30 18:25:46 +1100

    add basic layout of map sidebar

--- frontend/src/app/map-sidebar/map-sidebar.component.ts
+++ frontend/src/app/map-sidebar/map-sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MapService} from '../services/map.service';
+import {HotelSideBarInfo} from './hotelSideBarInfo';
 
 @Component({
   selector: 'app-map-sidebar',
@@ -9,12 +10,25 @@ import {MapService} from '../services/map.service';
 })
 export class MapSidebarComponent implements OnInit {
 
-  private result: any;
-  constructor(private mapService: MapService) { }
+  private defaultHotelsInfo: Array<HotelSideBarInfo> = [];
+  private resStr: string;
+  constructor(private mapService: MapService) {
+  }
 
   ngOnInit() {
     this.mapService.getAllHotels().subscribe(
-      res => this.result = res
+      res => {
+        this.resStr = JSON.stringify(res);
+        JSON.parse(this.resStr).res.forEach((obj) => {
+          const hotel = new HotelSideBarInfo('', -1, '', '');
+          hotel.description = obj.description;
+          hotel.id = obj.id;
+          hotel.location = obj.location;
+          hotel.name = obj.name;
+          this.defaultHotelsInfo.push(hotel);
+        });
+        // console.log(this.defaultHotelsInfo);
+      }
     );
   }
 
