d0aef8bbee7ce00cb34ed10787c0833ef8d21d43
ARnnnooo51 <fanchenming51@gmail.com>
2019-11-01 12:42:59 +1100

    save for search

---
+++ frontend/src/app/homepage/SearchInfo.ts
@@ -0,0 +1,8 @@
+export class SearchInfo {
+  constructor(
+    public location: string,
+    // tslint:disable-next-line:variable-name
+    public room_type: string,
+    // tslint:disable-next-line:variable-name
+  ) {}
+}
--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {SearchInfo} from './SearchInfo';
+import {SearchService} from '../services/search.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  providers: [SearchService]
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  private searchInfo: SearchInfo;
 
-  ngOnInit() {
+  constructor(private searchService: SearchService) {
+    this.searchInfo = new SearchInfo('', 'Large');
   }
 
+  ngOnInit() {
+
+  }
+
+  public onSearchSubmit(): void {
+    console.log(this.searchInfo.location);
+    this.searchService.search(this.searchInfo).subscribe(
+      res => console.log(res)
+    );
+  }
 }
