f65099b22a003cd55ec2e8e0e8f06081cfc80113
Toby Zheng <tobbyzheng0807@gmail.com>
2019-11-05 18:09:40 +1100

    basic chatbot ui done

--- frontend/src/app/chatbot/chatbot.component.ts
+++ frontend/src/app/chatbot/chatbot.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {ChatbotService} from '../services/chatbot.service';
-import { ChatMsg } from './chatMsg';
+import { Component } from '@angular/core';
+
 import { Subject, from, merge, Observable } from 'rxjs';
 import { switchMap, map, windowCount, scan, take, tap } from 'rxjs/operators';
 
 import { ChatModule, Message, User, Action, ExecuteActionEvent, SendMessageEvent } from '@progress/kendo-angular-conversational-ui';
+import {ChatService} from '../services/chat.service';
 
 @Component({
   selector: 'app-chatbot',
   templateUrl: './chatbot.component.html',
   styleUrls: ['./chatbot.component.css'],
-  providers: [ChatbotService]
+  providers: [ChatService]
 })
-export class ChatbotComponent{
+export class ChatbotComponent {
 
   // private userInput: string;
   // private chatMsg: ChatMsg = new ChatMsg('');
   // public response: {} = '';
+
   public feed: Observable<Message[]>;
 
   public readonly user: User = {
@@ -29,55 +30,44 @@ export class ChatbotComponent{
 
   private local: Subject<Message> = new Subject<Message>();
 
-  constructor(private chatbotService: ChatbotService) {
+  constructor(private svc: ChatService) {
     const hello: Message = {
       author: this.bot,
-      suggestedActions: [{
-        type: 'reply',
-        value: 'Neat!'
-      }, {
-        type: 'reply',
-        value: 'Thanks, but this is boring.'
-      }],
+      // suggestedActions: [{
+      //   type: 'reply',
+      //   value: 'Neat!'
+      // }, {
+      //   type: 'reply',
+      //   value: 'Thanks, but this is boring.'
+      // }],
       timestamp: new Date(),
       text: 'Hello, this is a demo bot. I don`t do much, but I can count symbols!'
     };
 
-// // Merge local and remote messages into a single stream
-//     this.feed = merge(
-//       from([ hello ]),
-//       this.local,
-//       this.svc.responses.pipe(
-//         map((response): Message => ({
-//           author: this.bot,
-//           text: response
-//         })
-//       ))
-//     ).pipe(
-//       // ... and emit an array of all messages
-//       scan((acc: Message[], x: Message) => [...acc, x], [])
-//     );
-//   }
+    // Merge local and remote messages into a single stream
+    this.feed = merge(
+      from([ hello ]),
+      this.local,
+      this.svc.responses.pipe(
+        map((response): { author: User; text: unknown } => ({
+            author: this.bot,
+            text: response
+          })
+        ))
+    ).pipe(
+      // ... and emit an array of all messages
+      scan((acc: Message[], x: Message) => [...acc, x], [])
+    );
+  }
 
-//   public sendMessage(e: SendMessageEvent): void {
-//     this.local.next(e.message);
+  public sendMessage(e: SendMessageEvent): void {
+    this.local.next(e.message);
 
-//     this.local.next({
-//       author: this.bot,
-//       typing: true
-//     });
+    this.local.next({
+      author: this.bot,
+      typing: true
+    });
 
-//     this.svc.submit(e.message.text);
-//   }
-
-  // onClick(): void {
-  //   this.chatMsg.message = this.userInput;
-  //   this.chatbotService.sendMsg(this.chatMsg).subscribe(
-  //     res => {
-  //       this.response = res;
-  //       console.log('response:' + this.response);
-  //     }
-  //   );
-
+    this.svc.submit(e.message.text);
   }
 }
--- frontend/src/app/services/chat.service.spec.ts
+++ frontend/src/app/services/chat.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ChatbotService } from './chatbot.service';
+import { ChatService } from './chat.service';
 
-describe('ChatbotService', () => {
+describe('ChatService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: ChatbotService = TestBed.get(ChatbotService);
+    const service: ChatService = TestBed.get(ChatService);
     expect(service).toBeTruthy();
   });
 });
