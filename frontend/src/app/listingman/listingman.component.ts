8f101c37700880f6cb9d4dcec267bcd32747500a
Toby Zheng <tobbyzheng0807@gmail.com>
2019-11-16 22:53:12 +1100

    tmp save

--- frontend/src/app/listingman/listingman.component.ts
+++ frontend/src/app/listingman/listingman.component.ts
@@ -1,10 +1,11 @@
-import {Component, Inject, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, EventEmitter, Inject, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {EditFormService} from '../services/edit-form.service';
 import { GridDataResult } from '@progress/kendo-angular-grid';
 import {State, process, SortDescriptor} from '@progress/kendo-data-query';
 import {Observable} from 'rxjs';
 import {ListingInfo} from './listingInfo';
 import { map } from 'rxjs/operators';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-listingman',
@@ -15,16 +16,26 @@ import { map } from 'rxjs/operators';
 })
 export class ListingmanComponent implements OnInit {
 
-  // public editListing: ListingInfo;
-  // public isNew: boolean;
+  public curHotelName: string;
+  public curHotelId: string;
   public showRoom = false;
-  public active = false;
   public token;
-  // public listingArray: Array<ListingInfo>;
   public gridData: GridDataResult;
   public view: Observable<GridDataResult>;
-  public curEditAction = 'Edit';
-  public curHotelName: string;
+  // public roomGridData: GridDataResult;
+  // public roomView: Observable<GridDataResult>
+  // Edit form variables:
+  public isNew: boolean;
+  public active = false;
+  public editListing: ListingInfo;
+  public editHotelForm: FormGroup = new FormGroup({
+    hotel_name: new FormControl(),
+    hotel_address: new FormControl(),
+    phone: new FormControl(),
+    email: new FormControl(),
+    description: new FormControl()
+  });
+
   constructor(private editService: EditFormService) {
     this.view = editService;
   }
@@ -42,18 +53,83 @@ export class ListingmanComponent implements OnInit {
     });
   }
 
+  public getAllRoomData(hotelId: string): void {
+    this.token = window.localStorage.getItem('token');
+    this.editService.readRoom(this.token, hotelId);
+    this.view.subscribe(res => {
+      console.log('in get all room data');
+      console.log(res);
+      this.gridData = res;
+    });
+  }
+
+  // Hotel grid three buttons handlers:
   public onShowRooms(dataItem): void {
     this.showRoom = true;
     this.curHotelName = dataItem.hotel_name;
+    this.curHotelId = dataItem.hotel_id;
+    this.getAllRoomData(this.curHotelId);
   }
 
   public onRemoveHotel({dataItem}): void {
     console.log(dataItem);
+    this.editService.remove(dataItem.hotel_id, this.token).subscribe(() =>
+      window.location.reload()
+    );
   }
 
   public onEditHotel({rowIndex, dataItem}): void {
     console.log(rowIndex);
     console.log(dataItem);
+    this.isNew = false;
+    // tslint:disable-next-line:max-line-length
+    this.editListing = new ListingInfo(dataItem.description, dataItem.email, '', dataItem.hotel_address, dataItem.hotel_id, dataItem.hotel_name, dataItem.phone, '', []);
+    this.active = true;
+    this.editHotelForm.reset(this.editListing);
   }
+
+  // Edit Hotel Form handlers:
+  public addNewHotel(): void {
+    this.isNew = true;
+    this.editListing = new ListingInfo('', '', '', '', '', '', '', '', []);
+    this.active = true;
+    this.editHotelForm.reset(this.editListing);
+  }
+
+  public closeHotelForm(): void {
+    this.active = false;
+  }
+
+  public cancelHotelForm(): void {
+    this.closeHotelForm();
+  }
+
+  public saveHotelForm(): void {
+    // console.log(this.editHotelForm.value);
+    this.editListing.hotel_name = this.editHotelForm.value.hotel_name;
+    this.editListing.hotel_address = this.editHotelForm.value.hotel_address;
+    this.editListing.description = this.editHotelForm.value.description;
+    this.editListing.email = this.editHotelForm.value.email;
+    this.editListing.phone = this.editHotelForm.value.phone;
+    this.editService.saveHotel(this.editListing, this.token, this.isNew).subscribe(res => {
+      console.log(res);
+      this.closeHotelForm();
+      window.location.reload();
+    });
+  }
+
+  // Room Grid button handlers:
+  public onEditRoom({dataItem}): void {
+
+  }
+
+  public onRemoveRoom({dataItem}): void {
+
+  }
+
+  public addNewRoom(): void {
+
+  }
+
 
 }
--- frontend/src/app/services/edit-form.service.ts
+++ frontend/src/app/services/edit-form.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
 import { tap, map } from 'rxjs/operators';
 import {GridDataResult} from '@progress/kendo-angular-grid';
+import {ListingInfo} from '../listingman/listingInfo';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class EditFormService extends BehaviorSubject<GridDataResult> {
 
   url = 'http://nomoreprojectpls.com';
   path = '/hotel/management';
+  roomPath = '/room/management';
   loading = false;
   constructor(private http: HttpClient) {
     super(null);
@@ -39,14 +41,60 @@ export class EditFormService extends BehaviorSubject<GridDataResult> {
       tap(() => this.loading = false));
   }
 
+  public readRoom(token: string, hotelId: string) {
+    this.getAllRoom(token, hotelId).subscribe(dt => super.next(dt));
+  }
+
+  public getAllRoom(token: string, hotelId: string) {
+    this.loading = true;
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token,
+      }),
+      body: {
+        hotel_id: hotelId
+      }
+    };
+    return this.http.get(this.url + this.roomPath, options).pipe(map(res => ({
+      // @ts-ignore
+      data: res.res,
+      total: 100
+      } as GridDataResult)),
+      tap(() => this.loading = false));
+  }
 
   // http post and put
-  public save() {
+  public saveHotel(hotel: ListingInfo, token: string, isNew: boolean) {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+    const body = JSON.stringify(hotel);
+    if (isNew) {
+      return this.http.post(this.url + this.path, body, options);
+    } else {
+      console.log(body);
+      return this.http.put(this.url + this.path, body, options);
+    }
 
   }
 
   // http delete
-  public remove() {
+  public remove(hotelId: string, token: string) {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      }),
+      body: {
+        hotel_id: hotelId
+      }
+    };
 
+    console.log(options);
+    return this.http.delete(this.url + this.path, options);
   }
 }
